ca44c9e3bb77e3a1faa62552f71fdf7194ca1e5f
Alaa-AbdElmotlep <alaa.abdelmotlep.mahmoed@gmail.com>
2024-03-16 22:40:31 +0200

    Some enhancement

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,102 +1,109 @@
-import { useState } from 'react';
-import { Api } from '../services/api';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from "react";
+import { Api } from "../services/api";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const { VITE_AI_MODULE_BASE_URL } = import.meta.env;
 
 const LoginPage = () => {
   const [credentials, setCredentials] = useState({
-    username: '',
-    password: '',
+    username: "",
+    password: "",
   });
   const [showPassword, setShowPassword] = useState(false);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setCredentials({ ...credentials, [name]: value });
+    setCredentials((prevCredentials) => ({ ...prevCredentials, [name]: value }));
   };
 
   const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleLoginSuccess = (accessToken) => {
+    localStorage.setItem("token", accessToken);
+    toast.success("Login successful!");
+    // Redirect user after successful login
+    window.location.href = VITE_AI_MODULE_BASE_URL;
+  };
+
+  const handleLoginFailure = (error) => {
+    if (error.response) {
+      const { message } = error.response.data;
+      toast.error(message);
+    } else {
+      toast.error("An error occurred. Please try again.");
+    }
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       const response = await Api.post(
-        '/auth/login',
+        "/auth/login",
         { email: credentials.username, password: credentials.password },
-        { headers: { 'Content-Type': 'application/json' } }
+        { headers: { "Content-Type": "application/json" } }
       );
       const { access_token } = response.data;
-      localStorage.setItem('userToken', access_token);
-      toast.success('Login successful!');
+      handleLoginSuccess(access_token);
     } catch (error) {
-      if (error.response) {
-        const errorMessage = error.response.data.message;
-        toast.error(errorMessage);
-      } else {
-        toast.error('An error occurred. Please try again.');
-      }
+      handleLoginFailure(error);
     }
   };
 
   return (
-    <div className='flex items-center justify-center min-h-screen bg-gray-50 px-4'>
-      <div className='max-w-md w-full bg-white shadow-md overflow-hidden rounded-xl'>
-        <div className='px-6 py-8'>
-          <h2 className='text-2xl font-bold mb-4 '>Sign In To Your Account</h2>
-          <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 px-4">
+      <div className="max-w-md w-full bg-white shadow-md overflow-hidden rounded-xl">
+        <div className="px-6 py-8">
+          <h2 className="text-2xl font-bold mb-4">Sign In To Your Account</h2>
+          <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
             <div>
-              <label htmlFor='username' className='mb-2 block'>
+              <label htmlFor="username" className="mb-2 block">
                 Username
               </label>
               <input
-                id='username'
-                name='username'
-                type='text'
+                id="username"
+                name="username"
+                type="text"
                 required
-                className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500'
-                placeholder='Username'
+                value={credentials.username}
                 onChange={handleInputChange}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                placeholder="Username"
               />
             </div>
             <div>
-              <label htmlFor='password' className='mb-2 block'>
+              <label htmlFor="password" className="mb-2 block">
                 Password
               </label>
-
-              <div className='flex items-center'>
+              <div className="flex items-center">
                 <input
-                  id='password'
-                  name='password'
-                  type={showPassword ? 'text' : 'password'}
+                  id="password"
+                  name="password"
+                  type={showPassword ? "text" : "password"}
                   required
-                  className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500'
-                  placeholder='Password'
+                  value={credentials.password}
                   onChange={handleInputChange}
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                  placeholder="Password"
                 />
                 <button
-                  type='button'
-                  className='bg-[#E5E7EB] p-2 rounded-md ml-3'
+                  type="button"
+                  className="bg-[#E5E7EB] p-2 rounded-md ml-3"
                   onClick={togglePasswordVisibility}
                 >
-                  {showPassword ? (
-                    <div className='p-1'>
-                      <img className='' src='eye-closed.svg' alt='' />
-                    </div>
-                  ) : (
-                    <div className='p-1'>
-                      <img className='' src='eye-open.svg' alt='' />
-                    </div>
-                  )}
+                  <img
+                    className="p-1"
+                    src={showPassword ? "eye-closed.svg" : "eye-open.svg"}
+                    alt=""
+                  />
                 </button>
               </div>
             </div>
-
             <div>
               <button
-                type='submit'
-                className='w-full py-2 px-4 font-semibold text-white rounded-md bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50'
+                type="submit"
+                className="w-full py-2 px-4 font-semibold text-white rounded-md bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
               >
                 Sign in
               </button>
